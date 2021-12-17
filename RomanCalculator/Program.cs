9aea1fadcd752398d0ccddcfc7350aae8ff7925c
todd flanders <toddfbass@gmail.com>
2021-12-17 12:49:02 -0500

    add dummy console app

---
+++ RomanCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Roman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
